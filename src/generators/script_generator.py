2d99160e763099dd51df5082139adfdf528e2240
Anshul <webserieswatchdog@gmail.com>
2026-06-23 05:14:26 +0530

    Create script_generator.py

---
+++ src/generators/script_generator.py
@@ -0,0 +1,18 @@
+"""
+Script generation module.
+"""
+
+def generate_script(topic):
+    return f"""
+Introduction
+
+Today we discuss {topic}.
+
+Main Content
+
+This is a generated script about {topic}.
+
+Conclusion
+
+Thanks for watching.
+"""
