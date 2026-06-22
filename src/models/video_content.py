4716a6a70d395527df00d833a317d066a15a18f4
Anshul <webserieswatchdog@gmail.com>
2026-06-23 05:16:35 +0530

    Create video_content.py

---
+++ src/models/video_content.py
@@ -0,0 +1,16 @@
+class VideoContent:
+    def __init__(
+        self,
+        title,
+        description,
+        tags,
+        thumbnail_prompt,
+        outline,
+        script,
+    ):
+        self.title = title
+        self.description = description
+        self.tags = tags
+        self.thumbnail_prompt = thumbnail_prompt
+        self.outline = outline
+        self.script = script
